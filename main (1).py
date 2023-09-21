3aebd89daabcb9caa044d469dd20bd1ce7b9e585
bdu3002022it01 <144235869+bdu3002022it01@users.noreply.github.com>
2023-09-21 21:58:40 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,22 +1,36 @@
-# define base class player
-class player(self):
-def play(slef):
-print("The player is playing cricket.")
 
-# define derived class batsman
-class Batsman(player):
-def play(self):
-print("The batsman is batting.")
 
-# define derived class bowler
-class Bowler(player):
-def play(self):
-print("The bowler is bowling.")
 
-# create object of batsman and bowler classes
-batsman = Batsman()
-bowler = Bowler()
 
-# call play() method
-batsman.play()
-bowler.play()
+
+
+ 
+
+
+class Student:
+
+  def __init__(self, name, roll_number, cgpa): 
+    self.name = name
+    self. roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+
+# Sort the list of students in descending orders of cgpa
+
+sorted_students = sorted(student_list, key=lambda Student: Student.cgpa,reverse=True)
+
+#syntax - lambda arg:exp
+return sorted_students
+
+
+# example
+Students = [Student("Abi","AB1",7.8),
+            Student("Ram","AB2",8.9),
+            Student("Mani","AB3",9.1),
+            Student("Abar","AB4",9.9),]
+Sorted_Students = Sort_Students(Students)
+# print
+for Student in Sorted_Students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(Student.name,Student.roll_number,Student.cgpa))
